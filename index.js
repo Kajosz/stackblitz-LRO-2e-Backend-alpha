6ad29d70c91089a5b1c4e0774b38e6b3fc8845e3
Kajosz <kajetan.szczurowski@gmail.com>
2023-12-16 21:53:43 +0100

    socket

--- index.js
+++ index.js
@@ -58,7 +58,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"]
+  }
+});
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
