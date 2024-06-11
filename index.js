3a2aca47811f4398725e61c39860b8b70a8215a5
Kajetan Szczurowski <72921975+kajetan-szczurowski@users.noreply.github.com>
2024-06-11 19:27:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"]
+    origin: ["https://lets-roll-one.netlify.app"]
   }
 });
 const PORT = process.env.PORT || 3000;
